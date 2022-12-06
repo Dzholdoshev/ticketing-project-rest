85ca3f1eabf0af2151ee40e43b107701c5acee7d
marklen <marklen86@gmail.com>
2022-12-05 21:33:05 -0600

    changes

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -17,26 +19,31 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/list")
+    @GetMapping()
+    @RolesAllowed({"Manager","Admin"})
     public ResponseEntity<ResponseWrapper> getUsers(){
         return  ResponseEntity.ok(new ResponseWrapper("Users found",userService.listAllUsers(), HttpStatus.ACCEPTED));
     }
     @GetMapping("/{username}")
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String name){
         return ResponseEntity.ok(new ResponseWrapper("User by name found", userService.findByUserName(name),HttpStatus.OK));
     }
     @PostMapping()
+    @RolesAllowed("Admin")
     public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user){
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseWrapper("User created", HttpStatus.CREATED));
     }
     @PutMapping
+    @RolesAllowed({"Admin"})
     public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user){
         userService.save(user);
         return ResponseEntity.ok(new ResponseWrapper("user updated",HttpStatus.OK));
     }
     @DeleteMapping("/{username}")
+    @RolesAllowed({"Admin"})
     public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String name){
         userService.delete(name);
         return ResponseEntity.ok( new ResponseWrapper("User is deleted", HttpStatus.OK));
